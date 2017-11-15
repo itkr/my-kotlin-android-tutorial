32ff946bd37d1cf930dac36ff30b7e5888e65c0c
itkr <itkrst@gmail.com>
2017-11-15 20:43:45 +0900

    Qiitaから検索

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -2,7 +2,17 @@ package sample.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 
@@ -22,6 +32,31 @@ class MainActivity : AppCompatActivity() {
         listView.setOnItemClickListener { adapterView, view, position, id ->
             val article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
+        }
+
+        val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById<EditText>(R.id.query_edit_text)
+        val searchButton = findViewById<Button>(R.id.search_button)
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    }, {
+                        toast("エラー: $it")
+                    })
         }
     }
 
