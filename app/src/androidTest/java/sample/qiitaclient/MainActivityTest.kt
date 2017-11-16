ea3ac62701b0c619372828825a6a1b1dc7f70821
itkr <itkrst@gmail.com>
2017-11-16 17:46:59 +0900

    モックによるテストの追加

--- app/src/androidTest/java/sample/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/java/sample/qiitaclient/MainActivityTest.kt
@@ -1,6 +1,8 @@
 package sample.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -12,6 +14,11 @@ import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import rx.Observable
+import sample.qiitaclient.client.ArticleClient
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -27,6 +34,19 @@ class MainActivityTest {
         onView(withId(R.id.search_button)).check(matches(isDisplayed()))
 
         onView(withId(R.id.progress_bar)).check(matches(isNotDisplayed()))
+    }
+
+    @Test
+    fun `検索ボタンがタップされたら`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:itkr")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:itkr"))
+        onView(withId(R.id.search_button)).perform(click())
+
+        verify(articleClient).search("user:itkr")
     }
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
